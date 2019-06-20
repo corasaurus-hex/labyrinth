4155d09481c96be9cb4523b0395b80751ee34539
Cora Sutton <cora@sutton.me>
2019-06-19 22:01:54 -0500

    Strip the nonsense out of core

--- src/labyrinth/core.clj
+++ src/labyrinth/core.clj
@@ -1,6 +1,1 @@
-(ns labyrinth.core
-  (:require [labyrinth.grid :as grid]
-            [labyrinth.grid.binary-tree :as binary-tree]))
-
-(grid/->maze 5 5)
-(binary-tree/random-direction)
+(ns labyrinth.core)
