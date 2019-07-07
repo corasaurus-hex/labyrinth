12e2ab4c6443d572c10d6b8e0896cb4cc17e28c7
Nathan Sutton <nate@sutton.me>
2019-07-06 23:39:39 -0500

    Improve the correctness of the binary tree algorithm

--- src/labyrinth/grid/binary_tree.clj
+++ src/labyrinth/grid/binary_tree.clj
@@ -6,15 +6,27 @@
   []
   (rand-nth [:north :east]))
 
+(defn end-coord?
+  [{:keys [width height]} [col row]]
+  (and (= col width) (= row height)))
+
 (defn at-end?
   "Are we at the end of the maze, meaning the walk function should stop."
-  [{:keys [width height] [col row] :cursor}]
-  (and (= col width) (= row height)))
+  [{:keys [cursor] :as maze}]
+  (end-coord? maze cursor))
+
+(defn next-cursor-pos
+  "Gets the next position the walk function should move the cursor to. Returns nil if at the end of the maze."
+  [{:keys [width] [col row] :cursor :as maze}]
+  (when-not (at-end? maze)
+    (if (= col width)
+      [1 (inc row)]      ;; at the right edge of the grid, move all the way left and up a row
+      [(inc col) row]))) ;; not at the right edge yet, keep row the same and move right
 
 (defn penultimate?
   "Are we at the penultimate cell of the maze, meaning at-end? returns true for the next cell."
-  [{:keys [width height] [col row] :cursor}]
-  (and (= (inc col) width) (= row height)))
+  [maze]
+  (end-coord? maze (next-cursor-pos maze)))
 
 (defn next-direction-to-link
   "Gets the next coordinate to link the cursor to. Returns nil if at the end of the maze."
@@ -26,14 +38,6 @@
         max-row :east
         max-col :north
         :else (random-direction)))))
-
-(defn next-cursor-pos
-  "Gets the next position the walk function should move the cursor to. Returns nil if at the end of the maze."
-  [{:keys [width] [col row] :cursor :as maze}]
-  (when-not (at-end? maze)
-    (if (= col width)
-      [1 (inc row)]      ;; at the right edge of the grid, move all the way left and up a row
-      [(inc col) row]))) ;; not at the right edge yet, keep row the same and move right
 
 (defn next-steps
   "Returns the next list of of operations to perform on the maze"
