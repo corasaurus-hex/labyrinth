4a89dd3f9f0d76b712433786e6e34606ec823f22
Cora Sutton <cora@sutton.me>
2019-07-07 00:04:05 -0500

    Even more tests for the binary tree algorithm

--- test/labyrinth/grid/binary_tree_test.clj
+++ test/labyrinth/grid/binary_tree_test.clj
@@ -11,3 +11,45 @@
                 (take 1000)
                 (distinct)
                 (sort))))))
+
+(deftest end-coord?
+  (testing "returns true when the coord is the end coord"
+    (is (b/end-coord? (g/->maze 3 3) [3 3])))
+  (testing "returns false when the coord is not the end coord"
+    (is (not (b/end-coord? (g/->maze 3 3) [1 3])))))
+
+(deftest at-end?
+  (testing "returns true when the cursor is the northwestern-most coordinate"
+    (is (-> (g/->maze 3 3)
+            (g/move-cursor [3 3])
+            (b/at-end?))))
+  (testing "returns false when the cursor is not at the northwestern-most coordinate"
+    (is (not (-> (g/->maze 3 3)
+                 (g/move-cursor [2 3])
+                 (b/at-end?))))))
+
+(deftest next-cursor-pos
+  (testing "returns the next cursor position in the grid when there is one"
+    (is (= [2 1] (b/next-cursor-pos (g/->maze 3 3))))
+    (is (= [1 2] (b/next-cursor-pos (g/move-cursor (g/->maze 3 3) [3 1]))))
+    (is (= [1 3] (b/next-cursor-pos (g/move-cursor (g/->maze 3 3) [3 2]))))
+    (is (= [3 3] (b/next-cursor-pos (g/move-cursor (g/->maze 3 3) [2 3]))))
+    (is (= [1 3] (b/next-cursor-pos (g/move-cursor (g/->maze 1 3) [1 2]))))
+    (is (= [3 1] (b/next-cursor-pos (g/move-cursor (g/->maze 3 1) [2 1])))))
+  (testing "returns nil when there is no next cursor position in the grid"
+    (is (nil? (b/next-cursor-pos (g/move-cursor (g/->maze 3 3) [3 3]))))
+    (is (nil? (b/next-cursor-pos (g/move-cursor (g/->maze 1 3) [1 3]))))
+    (is (nil? (b/next-cursor-pos (g/move-cursor (g/->maze 3 1) [3 1]))))))
+
+(deftest penultimate?
+  (testing "returns true when at the coordinate before the end coordinate"
+    (is (b/penultimate? (g/move-cursor (g/->maze 3 3) [2 3])))
+    (is (b/penultimate? (g/move-cursor (g/->maze 1 3) [1 2])))
+    (is (b/penultimate? (g/move-cursor (g/->maze 3 1) [2 1]))))
+  (testing "returns false when not at the coordinate before the end coordinate"
+    (is (not (b/penultimate? (g/move-cursor (g/->maze 3 3) [3 3]))))
+    (is (not (b/penultimate? (g/move-cursor (g/->maze 3 3) [1 3]))))
+    (is (not (b/penultimate? (g/move-cursor (g/->maze 1 3) [1 3]))))
+    (is (not (b/penultimate? (g/move-cursor (g/->maze 1 3) [1 1]))))
+    (is (not (b/penultimate? (g/move-cursor (g/->maze 3 1) [3 1]))))
+    (is (not (b/penultimate? (g/move-cursor (g/->maze 3 1) [1 1]))))))
