35b8bb1ca90e597c9753d229397e72e8b03ee2f5
Cora Sutton <cora@sutton.me>
2019-07-15 20:36:42 -0500

    Add a fiddle file in the dev directory for storing helpful functions and snippets

--- src/labyrinth/core.clj
+++ src/labyrinth/core.clj
@@ -1,6 +1,1 @@
 (ns labyrinth.core)
-(require '[labyrinth.grid :as g]
-         '[labyrinth.grid.export :as e]
-         '[labyrinth.grid.export.ir :as ir]
-         '[labyrinth.grid.binary-tree :as bt]
-         '[labyrinth.grid.specs :as s])
